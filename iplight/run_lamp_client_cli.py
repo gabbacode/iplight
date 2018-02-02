e9318a2a4dcf8a5e4b5476407285f91ee7d9cb2a
Sergey Kochekovsky <sergey.a.kochekovsky@gmail.com>
2018-02-02 04:39:55 +0300

    -sigterm

--- iplight/run_lamp_client_cli.py
+++ iplight/run_lamp_client_cli.py
@@ -1,9 +1,10 @@
 import asyncio
+import signal
+import sys
 from iplight.arguments import get_arguments
 from iplight.lamp import LampRepresentation
 from iplight.lamp import LampController
 from iplight.lamp_client import LampClient
-from iplight.loop_helper import run_loop_able_to_break
 
 
 class LampTextView(LampRepresentation):
@@ -15,6 +16,25 @@ class LampTextView(LampRepresentation):
             .format(
                 "on" if state.is_on else "off",
                 state.color))
+
+
+class InterruptedExit(SystemExit):
+    code = 1
+
+
+def run_loop_able_to_break(loop, action):
+
+    def _signal_handler():
+        raise InterruptedExit()
+
+    if signal is not None and sys.platform != 'win32':
+        loop.add_signal_handler(signal.SIGINT, _signal_handler)
+    try:
+        action(loop)
+    finally:
+        if signal is not None and sys.platform != 'win32':
+            loop.remove_signal_handler(signal.SIGINT)
+        loop.close()
 
 
 if __name__ == '__main__':
